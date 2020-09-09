eb9ce206bc68716053495e890129bf0603c9cbe1
Ryan K Burns <ryankburns@gmail.com>
2020-09-09 19:25:29 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,44 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+display_board(board)
+
+
+def valid_move?(board, index)
+  if index.to_i.between?(0, 8) && !position_taken?(board, index)
+    true
+  else
+    false
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  else
+    true
+  end
+end
+
+
+def input_to_index(input)
+input.to_i - 1
+end
+
+
+def move(board, index, character = "X")
+  board[index] = character
+end
